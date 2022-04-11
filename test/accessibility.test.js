9502420dc8ebab4b258ee3f360d3947a3d0c4f65
Theo Gainey <theo@heysparkbox.com>
2022-04-11 09:54:22 -0400

    test: rework accessibility.test.js
    rework test for accessibility options
    
    Changes to be committed:
    	modified:   test/accessibility.test.js

--- test/accessibility.test.js
+++ test/accessibility.test.js
@@ -1,52 +1,45 @@
-import { assert, assertEquals } from 'https://deno.land/std@0.128.0/testing/asserts.ts';
-import { wordWrap } from '../src/accessibility.js'
+import { assertEquals } from 'https://deno.land/std@0.128.0/testing/asserts.ts';
+import { wordWrap } from '../src/accessibility.js';
 
 Deno.test('wordWrap splits a string into an array of lines ', () => {
-const input = 'Sometimes science is more art than science, Morty. A lot of people dont get that';
-const expected = [
+  const input =
+    'Sometimes science is more art than science, Morty. A lot of people dont get that';
+  const expected = [
     'Sometimes science is more art',
     'than science, Morty. A lot of',
-    'people dont get that'
+    'people dont get that',
   ];
   const actual = wordWrap(input, 30);
   assertEquals(actual, expected);
 });
 
-Deno.test('wordWrap places words longer than max line length on their own line', () => { 
-  const input = 'Sometimes, the elegant implementation is a function. Not a method. Not a class. Not a framework. Just a function';
-  const result = wordWrap(input, 12);
+Deno.test('wordWrap places words longer than max line length on their own line', () => {
+  const input = 'the word implementation is long';
+  const lineLenth = 12;
+  const result = wordWrap(input, lineLenth);
 
-  const wordsLongerThanMax = input.split(' ').filter((e) => e !==' ' && e.length >= 12);
-  const linesLongerThanMax = result.filter((e) => e.length > 12);
+  const expected = ['the word', 'implementation', 'is long'];
 
-  //number of lines longer than max length must be equal to words longer than max length 
-  assertEquals(wordsLongerThanMax.length, linesLongerThanMax.length);
-
-  //each line longer than max length must not have any spaces to contain only 1 word 
-  linesLongerThanMax.forEach((line) => {
-    const oneWord = !line.includes(' ');
-    assert(oneWord);
-  });
-  
+  assertEquals(result, expected);
 });
 
-Deno.test('wordWrap only includes one space between words', () => { 
+Deno.test('wordWrap only includes one space between words', () => {
   const input = 'Making    things      easy is    hard';
-  const expected = [ "Making things easy", "is hard" ];
+  const expected = ['Making things easy', 'is hard'];
   const actual = wordWrap(input, 20);
   assertEquals(actual, expected);
 });
 
-
-Deno.test('wordWrap does not split a string into multiple line if is not longer than the max line length', () => {  
-  const input = 'Program testing can be used to show the presence of bugs, but never to show their absence';
+Deno.test('wordWrap does not split a string into multiple lines if the string is not longer than the max line length', () => {
+  const input =
+    'Program testing can be used to show the presence of bugs, but never to show their absence';
   const actual = wordWrap(input, 9999);
   assertEquals(actual, [input]);
 });
 
-Deno.test('wordWrap returns only 1 line per word if every word is longer than max line limit', () => { 
+Deno.test('wordWrap returns only 1 line per word if every word is longer than max line limit', () => {
   const input = 'walla walla washington';
-  const expected = ['walla', 'walla', 'washington', ];
+  const expected = ['walla', 'walla', 'washington'];
   const actual = wordWrap(input, 4);
   assertEquals(actual, expected);
 });
