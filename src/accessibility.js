e85ed7e63bc2d5cc3140e92c50ecf8d4378f942c
Theo Gainey <theo@heysparkbox.com>
2022-04-15 10:46:50 -0400

    rework: wordWrap
    rework wordWrap to take a menuItem object as a parameter
    rework wordWrap tests to reflect new logic
    
    On branch feat--xbar-layout
     Changes to be committed:
    	modified:   src/accessibility.js
    	modified:   test/accessibility.test.js

--- src/accessibility.js
+++ src/accessibility.js
@@ -1,19 +1,26 @@
-//array access layer
+import {
+  pipe,
+  pipeCompatibleFilter,
+  pipeCompatibleMap,
+  pipeCompatibleReduce,
+  pipeCompatibleSplit,
+} from './utility.js';
+// array access layer
 const lastIndex = (arr) => arr.length - 1;
 const lastItem = (arr) => arr[lastIndex(arr)];
 
-//array element transform layer
+// array element transform layer
 const appendLastLine = (lines, word) => [
   ...lines.slice(0, lastIndex(lines)),
   lastItem(lines).concat(' ', word),
 ];
 const newLine = (lines, newLine) => lines.concat(newLine);
 
-//conditional logic layer
+// conditional logic layer
 const isUnderLimit = (line, word, lineLength) =>
   line.length + word.length + 1 <= lineLength || line === ''; //if line is empty always return true
 
-//array reducer
+// array reducer
 const reduceLines = (lineLength) =>
   (lines, word) => {
     return isUnderLimit(lastItem(lines), word, lineLength)
@@ -21,22 +28,39 @@ const reduceLines = (lineLength) =>
       : newLine(lines, word);
   };
 
-/**
- * transforms a string into an array of word wrapped lines
- * @param {string} text
- * @param {number} lineLength - max length of a line
- * @returns {string[]}
- */
-export const wordWrap = (text, lineLength) => {
-  return text.split(' ')
-    .filter((e) => e !== '')
-    .reduce(reduceLines(lineLength), [''])
-    .map((e) => e.trim());
-};
+// word wrap transformations
+const spiltToWords = pipeCompatibleSplit(' ');
+const removeExtraSpaces = pipeCompatibleFilter((word) => word !== '');
+
+const reduceToLines = (maxLineLength) =>
+  pipeCompatibleReduce(reduceLines(maxLineLength))(['']);
+
+const passPropertiesToLInes = (propertiesToPass) =>
+  pipeCompatibleMap((line) => ({
+    text: line.trim(),
+    ...propertiesToPass,
+  }));
+
+// apply transformations in order with pipe
+const applyWordWrap = ({ text, wordWrap, ...menuItemProperties }) =>
+  pipe(
+    spiltToWords,
+    removeExtraSpaces,
+    reduceToLines(wordWrap),
+    passPropertiesToLInes(menuItemProperties),
+  )(text);
+
+// entry layer conditional logic
+const hasWordWrap = (item) => !!item.wordWrap;
+// deno-lint-ignore no-unused-vars
+const ignoreWordWrap = ({ wordWrap, ...item }) => item;
+
+// entry layer
+export const wordWrap = (menuItem) =>
+  hasWordWrap(menuItem) ? applyWordWrap(menuItem) : [ignoreWordWrap(menuItem)];
 
 /**
  * returns true if XBARDarkMode is enabled.
- *
  * allways returns false if 'env' permission has not been previously granted.
  * @async
  * @returns {Promise<boolean>}
--- test/accessibility.test.js
+++ test/accessibility.test.js
@@ -1,45 +1,164 @@
 import { assertEquals } from 'https://deno.land/std@0.128.0/testing/asserts.ts';
 import { wordWrap } from '../src/accessibility.js';
 
-Deno.test('wordWrap splits a string into an array of lines ', () => {
-  const input =
-    'Sometimes science is more art than science, Morty. A lot of people dont get that';
+Deno.test('wordWrap splits a string into an array of lines', () => {
+  const input = {
+    text:
+      'Sometimes science is more art than science, Morty. A lot of people dont get that',
+    wordWrap: 30,
+  };
   const expected = [
-    'Sometimes science is more art',
-    'than science, Morty. A lot of',
-    'people dont get that',
+    {
+      text: 'Sometimes science is more art',
+    },
+    {
+      text: 'than science, Morty. A lot of',
+    },
+    {
+      text: 'people dont get that',
+    },
   ];
-  const actual = wordWrap(input, 30);
+  assertEquals(wordWrap(input), expected);
+});
+
+Deno.test('wordWrap keeps object feilds other than wordWrap constant', () => {
+  const input = {
+    text:
+      'Sometimes science is more art than science, Morty. A lot of people dont get that',
+    wordWrap: 30,
+    color: 'red',
+    size: 12,
+  };
+  const expected = [
+    {
+      text: 'Sometimes science is more art',
+      color: 'red',
+      size: 12,
+    },
+    {
+      text: 'than science, Morty. A lot of',
+      color: 'red',
+      size: 12,
+    },
+    {
+      text: 'people dont get that',
+      color: 'red',
+      size: 12,
+    },
+  ];
+  const actual = wordWrap(input);
   assertEquals(actual, expected);
 });
 
-Deno.test('wordWrap places words longer than max line length on their own line', () => {
-  const input = 'the word implementation is long';
-  const lineLenth = 12;
-  const result = wordWrap(input, lineLenth);
+Deno.test('wordWrap does not split words longer than the max line length into multiple lines', () => {
+  const input = {
+    text: 'the word implementation is long',
+    wordWrap: 12,
+  };
+  const result = wordWrap(input);
 
-  const expected = ['the word', 'implementation', 'is long'];
-
+  const expected = [
+    {
+      text: 'the word',
+    },
+    {
+      text: 'implementation',
+    },
+    {
+      text: 'is long',
+    },
+  ];
   assertEquals(result, expected);
 });
 
 Deno.test('wordWrap only includes one space between words', () => {
-  const input = 'Making    things      easy is    hard';
-  const expected = ['Making things easy', 'is hard'];
-  const actual = wordWrap(input, 20);
+  const input = {
+    text: 'Making    things      easy is    hard',
+    wordWrap: 20,
+  };
+  const expected = [
+    {
+      text: 'Making things easy',
+    },
+    {
+      text: 'is hard',
+    },
+  ];
+  const actual = wordWrap(input);
   assertEquals(actual, expected);
 });
 
 Deno.test('wordWrap does not split a string into multiple lines if the string is not longer than the max line length', () => {
-  const input =
-    'Program testing can be used to show the presence of bugs, but never to show their absence';
-  const actual = wordWrap(input, 9999);
-  assertEquals(actual, [input]);
+  const input = {
+    text:
+      'Program testing can be used to show the presence of bugs, but never to show their absence',
+    wordWrap: 9999,
+  };
+  const actual = wordWrap(input);
+  assertEquals(actual, [{ text: input.text }]);
 });
 
 Deno.test('wordWrap returns only 1 line per word if every word is longer than max line limit', () => {
-  const input = 'walla walla washington';
-  const expected = ['walla', 'walla', 'washington'];
-  const actual = wordWrap(input, 4);
+  const input = {
+    text: 'walla walla washington',
+    wordWrap: 4,
+  };
+  const expected = [
+    {
+      text: 'walla',
+    },
+    {
+      text: 'walla',
+    },
+    {
+      text: 'washington',
+    },
+  ];
+  const actual = wordWrap(input);
   assertEquals(actual, expected);
+});
+
+Deno.test('wordWrap does not return an object with a `wordWrap` field if input object contains a `wordWrap` field with a falsy value', () => {
+  const input = [
+    {
+      text: 'Code never lies, sometimes comments do',
+      wordWrap: false,
+    },
+    {
+      text: 'JavaScript making 0 falsy was a mistake',
+      wordWrap: 0,
+    },
+    {
+      text: 'undefined is well defined',
+      wordWrap: undefined,
+    },
+    {
+      text: 'intentional absence of a value should not carry pass this point',
+      wordWrap: null,
+    },
+    {
+      text: 'NaN does not equal NaN',
+      wordWrap: NaN,
+    },
+  ];
+  const expected = [
+    {
+      text: 'Code never lies, sometimes comments do',
+    },
+    {
+      text: 'JavaScript making 0 falsy was a mistake',
+    },
+    {
+      text: 'undefined is well defined',
+    },
+    {
+      text: 'intentional absence of a value should not carry pass this point',
+    },
+    {
+      text: 'NaN does not equal NaN',
+    },
+  ];
+  input.forEach((item, i) => {
+    assertEquals(wordWrap(item), [expected[i]]);
+  });
 });
