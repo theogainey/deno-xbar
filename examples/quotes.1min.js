9fe60be6485cbcabe261adfc71ed6541b3b9f80f
Theo Gainey <theo@heysparkbox.com>
2022-04-25 21:55:04 -0400

    docs: examples
    add examples

---
+++ examples/quotes.1min.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env /Users/theogainey/.deno/bin/deno run --allow-net --allow-env
+import {
+  isDarkMode,
+  separator,
+  xbar,
+} from 'https://deno.land/x/xbar@v0.3.0/mod.ts';
+
+const jsonResponse = await fetch(
+  'https://programming-quotes-api.herokuapp.com/quotes/random',
+);
+const jsonData = await jsonResponse.json();
+const darkMode = await isDarkMode();
+
+xbar([
+  {
+    text: 'Programming Quotes',
+  },
+  separator,
+  {
+    text: jsonData.author,
+    submenu: [
+      {
+        text: jsonData.en,
+        wordWrap: 40,
+        size: 16,
+        color: darkMode ? '#818cf8' : 'navy',
+      },
+    ],
+  },
+]);
